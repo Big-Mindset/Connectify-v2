881bf403756cf72fae260731916de8315a93b5ed
Big-Mindset <wadoodmemon0@gmail.com>
2026-03-23 15:36:38 +0500

    emoji picker added

--- frontend/src/app/hellllooooooooo/file.js
+++
@@ -1,2 +0,0 @@
-let variable = "wadood"
-console.log(variable)
---
+++ frontend/src/components/opened-chat/Emoji-Picker.jsx
@@ -0,0 +1,10 @@
+
+import data from '@emoji-mart/data'
+import { Picker } from 'emoji-mart'
+
+new Picker({ data })
+export default function EmojiPicker(){
+    return <div>
+
+    </div>
+}
